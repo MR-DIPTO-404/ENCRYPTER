5a76a4711958d73a84298e7d834b9d91065dac59
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2023-02-05 12:45:53 +0600

    Create PY3.py

---
+++ PY3.py
@@ -0,0 +1,8 @@
+from os import system as X
+import time
+X('clear')
+print('      FIRST FOLLOW MY GITHUB DEAR :)')
+X('xdg-open https://github.com/MR-DIPTO-404')
+time.sleep(5)
+X('clear')
+import PY3
