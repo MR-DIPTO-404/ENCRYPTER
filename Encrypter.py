4593e06df53774b98068845ca168f560c65476f2
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2023-03-11 18:16:59 +0600

    Delete Encrypter.py

--- Encrypter.py
+++
@@ -1,8 +0,0 @@
-from os import system as X
-import time
-X('clear')
-print('          FIRST FOLLOW MY GITHUB DEAR :)')
-time.sleep(5)
-X('xdg-open https://github.com/MR-DIPTO-404')
-X('clear')
-import Xx
